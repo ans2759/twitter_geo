2c0f1d496023e47c040f5ec92012a1b0a531f55b
ans2759 <ans2759@rit.edu>
2019-02-18 11:43:08 -0500

    fix yelp business display

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const fileUpload = require('express-fileupload');
 const app = express();
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public/images/favicon', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
--- yelp/yelpClient.js
+++ yelp/yelpClient.js
@@ -1,24 +1,35 @@
 const yelpAccess = require('../secure/yelpAccess');
 const yelp = require('yelp-fusion');
 const client = yelp.client(yelpAccess.api_key, {socketTimeout: 5000});
+const myCache = require('../utils/serviceCache');
+const SECONDS_IN_A_DAY = 86400;
+
+const YELP_BUSINESS_PREFIX = "YELP_BUSINESS_";
 
 exports.search = (term, latitude, longitude) => {
     return new Promise(((resolve, reject) => {
-        client.search({
-            term: term,
-            latitude: latitude,
-            longitude: longitude
-        }).then(response => {
-            console.log('Received response from yelp');
-            if (response.statusCode === 200) {
-                resolve(response.jsonBody.businesses);
-            } else {
-                console.log("Response status code " + response.statusCode + " returned from yelp");
-                reject("Error searching yelp");
-            }
-        }).catch(e => {
-            console.error(e);
-            reject("Error searching yelp");
+        myCache.get(YELP_BUSINESS_PREFIX + term).then((result) => {
+           if (result) {
+               resolve(result);
+           } else {
+               client.search({
+                   term: term,
+                   latitude: latitude,
+                   longitude: longitude
+               }).then(response => {
+                   console.log('Received response from yelp');
+                   if (response.statusCode === 200) {
+                        myCache.set(YELP_BUSINESS_PREFIX + term, response.jsonBody.businesses, SECONDS_IN_A_DAY);
+                       resolve(response.jsonBody.businesses);
+                   } else {
+                       console.log("Response status code " + response.statusCode + " returned from yelp");
+                       reject("Error searching yelp");
+                   }
+               }).catch(e => {
+                   console.error(e);
+                   reject("Error searching yelp");
+               });
+           }
         });
     }));
 };
